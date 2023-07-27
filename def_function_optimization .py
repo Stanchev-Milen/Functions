91ad7a54028c2dd3140bc27df735771e4d188158
Stanchev-Milen <140455334+Stanchev-Milen@users.noreply.github.com>
2023-07-27 12:45:33 +0300

    Add files via upload

---
+++ def_function_optimization .py
@@ -0,0 +1,29 @@
+from math import pi
+
+
+def figure_area(figure='figure could be: square, rectangle, circle or triangle'):
+    figure = input('Please give us the figure: ')
+    valid_values = ['square', 'rectangle', 'circle', 'triangle']
+    while figure in valid_values:
+        area = 0
+        if figure == 'square':
+            a = float(input('a = '))
+            area = a * a
+        elif figure == 'rectangle':
+            a = float(input('a = '))
+            b = float(input('b = '))
+            area = a * b
+        elif figure == 'circle':
+            r = float(input('r ='))
+            area = pi * (r * r)
+        elif figure == 'triangle':
+            a = float(input('a= '))
+            h = float(input('h= '))
+            area = (a * h) / 2
+
+        print(f'The area of the {figure} is {area: .3f}')
+        figure = input('Please give us the figure: ')
+
+
+figure_area()
+
